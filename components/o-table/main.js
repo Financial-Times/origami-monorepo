4b876e7c88f0a876a974de5eba57dc77fb13f440
chee <chee@snoot.club>
2021-08-31 17:46:59 +0100

    feat: let people know a table is active

--- components/o-table/main.js
+++ components/o-table/main.js
@@ -2,9 +2,12 @@ import oTable from './src/js/oTable.js';
 
 const constructAll = function() {
 	oTable.init();
+	window.alert("table activated");
 	document.removeEventListener('o.DOMContentLoaded', constructAll);
 };
 
 document.addEventListener('o.DOMContentLoaded', constructAll);
+
+
 
 export default oTable;

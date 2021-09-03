41eed30d00121d5a33887214fb84250ed109cf13
chee <chee@snoot.club>
2021-09-03 16:53:24 +0100

    chore: sleep for 30 seconds between components

--- scripts/workspaces.mjs
+++ scripts/workspaces.mjs
@@ -5,7 +5,8 @@ import {readPackage} from "read-pkg"
 let pkgJson = await readPackage()
 
 export const paths = () => glob(pkgJson.workspaces, {onlyDirectories: true})
-export const packages = async function (workspaces = paths) {
+
+export const packages = async function (workspaces = paths()) {
 	let packages = []
 	for await (let cwd of workspaces) {
 		packages.push(await readPackage({cwd}))

029830c231a5d1e979d20e47820f9368d755e8fb
chee <chee@snoot.club>
2021-09-03 12:57:26 +0100

    chore: tell repo data about new components

--- scripts/publish.mjs
+++ scripts/publish.mjs
@@ -1,11 +1,22 @@
 #!/usr/bin/env node
 import {$} from "zx"
+import {readFile} from "fs/promises"
 
 let outputs = JSON.parse(process.argv[2])
 
+let {REPO_DATA_KEY, REPO_DATA_SECRET} = process.env
+
 for (let key in outputs) {
 	let value = outputs[key]
-	let name = key.match(/^components\/(.*)--release_created$/)
-	if (!name || !value) continue
-	$`npm publish -w ${name[0].replace(/--release_created$/, '')}`
+	let match = key.match(/^components\/(.*)--release_created$/)
+	if (!match || !value) continue
+	let pkg = match[0].replace(/--release_created$/, "")
+	await $`npm publish -w ${pkg}`
+	let pkgjson = JSON.parse(await readFile(`components/${pkg}`, "utf-8"))
+	await $`curl -X POST \
+        -H 'Content-Type: application/json' -H 'X-Api-Key: ${REPO_DATA_KEY}' -H 'X-Api-Secret: "${REPO_DATA_SECRET}"' \
+        -d '{"packageName": "${pkg}", "version": "v${pkgjson.version}", "type":"npm"}' \
+        https://origami-repo-data-monorepo.herokuapp.com/v1/queue`
 }
+
+export {}

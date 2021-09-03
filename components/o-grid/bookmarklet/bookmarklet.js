1fae63fc90d998902a3cb9ea89ebeefc7acb6bc8
Jake Champion <me@jakechampion.name>
2021-09-03 14:20:32 +0100

    chore: commit fixes from eslint --fix

--- components/o-grid/bookmarklet/bookmarklet.js
+++ components/o-grid/bookmarklet/bookmarklet.js
@@ -3,13 +3,13 @@
 // Modified for o-grid by the Financial Times
 // http://alefeuvre.github.io/foundation-grid-displayer/
 (function() {
-	"use strict";
-	var startBookmarklet = function($) {
+
+	const startBookmarklet = function($) {
 
 		// Set bookmarklet framework
-		var scriptSrc  = $("script[src*='bookmarklet.js']").attr("src");
-		var framework = scriptSrc.split("?");
-		var gdFramework;
+		const scriptSrc = $("script[src*='bookmarklet.js']").attr("src");
+		const framework = scriptSrc.split("?");
+		let gdFramework;
 
 		if (framework.length > 1) {
 			gdFramework = framework[1];
@@ -20,20 +20,20 @@
 		$(window).on('resize', function() { setGridGutters(); });
 
 		// Close grid displayer
-		var removeGridDisplayer = function() {
+		const removeGridDisplayer = function() {
 			$("#grid-displayer-tools").remove();
 			$("#grid-displayer").remove();
-		},
+		};
 
 		// Build grid displayer
-		gdIsBuilt = false,
-		buildGridDisplayer = function(gridFramework) {
+		let gdIsBuilt = false;
+		const buildGridDisplayer = function(gridFramework) {
 
-			var $gdContainer = $("#grid-displayer .gd-container"),
-			$gdRow           = $("#grid-displayer .gd-row"),
-			$gdTools         = $("#grid-displayer-tools"),
-			colsHtml         = "",
-			gridNbcols       = parseInt($gdTools.find("#gdt-nbcols").val());
+			const $gdContainer = $("#grid-displayer .gd-container");
+			const $gdRow = $("#grid-displayer .gd-row");
+			const $gdTools = $("#grid-displayer-tools");
+			let colsHtml = "";
+			const gridNbcols = parseInt($gdTools.find("#gdt-nbcols").val());
 
 			if (gdIsBuilt) {
 				$gdContainer.removeClass().addClass("gd-container");
@@ -41,23 +41,23 @@
 				$gdTools.find(".framework-specific").hide();
 			}
 
-			for(var i = 0; i < gridNbcols; i++) {
+			for(let i = 0; i < gridNbcols; i++) {
 				colsHtml += "<div class=\"gd-column\">&nbsp;</div>";
 			}
 			$gdRow.append(colsHtml);
-			var $gdColumn = $gdRow.find(".gd-column");
+			const $gdColumn = $gdRow.find(".gd-column");
 
 			switch(gridFramework) {
 				case 'o4':
 					$gdContainer.addClass("o-grid-container");
 					$gdRow.addClass("o-grid-row");
 					$gdColumn.attr("data-o-grid-colspan", "1");
-				break;
+					break;
 				case 'so4':
 					$gdContainer.addClass("o-grid-container o-grid-container--snappy");
 					$gdRow.addClass("o-grid-row");
 					$gdColumn.attr("data-o-grid-colspan", "1");
-				break;
+					break;
 			}
 
 			$gdTools.css("display", "inline-block");
@@ -73,20 +73,20 @@
 				$("#grid-displayer").show();
 				gdIsBuilt = true;
 			}
-		},
+		};
 
 		// Setters
-		setGridColor = function(gridColor, gutterless) {
+		var setGridColor = function(gridColor, gutterless) {
 			$("#grid-displayer .gd-column").css("background-color", gridColor);
-		},
-		setGridOpacity = function(gridOpacity) {
+		};
+		var setGridOpacity = function(gridOpacity) {
 			$("#grid-displayer .gd-column").css("opacity", gridOpacity);
-		},
-		setGridGutters = function() {
-			var gutter = Origami['o-grid'].getCurrentGutter();
+		};
+		var setGridGutters = function() {
+			const gutter = Origami['o-grid'].getCurrentGutter();
 			$("#grid-displayer .gd-column").css({"border-width": "0 0 0 " + gutter, "border-style": "solid", "border-color": "#FFF", "padding": 0});
-		},
-		setGridZindex = function(gridZindex) {
+		};
+		var setGridZindex = function(gridZindex) {
 			$("#grid-displayer").css("z-index", gridZindex);
 		};
 
@@ -95,26 +95,26 @@
 		} else {
 
 			// Default parameters
-			var gdNbcols      = 12;
-			var gdColor       = "black";
-			var gdOpacity     = "0.3";
-			var gdZindex      = "999";
+			const gdNbcols = 12;
+			const gdColor = "black";
+			const gdOpacity = "0.3";
+			const gdZindex = "999";
 
 			// Frameworks list
-			var origamis = [["o4", "Origami Grid v4"], ["so4", "Origami Grid v4 (snappy)"]];
-			var frameworks = origamis;
+			const origamis = [["o4", "Origami Grid v4"], ["so4", "Origami Grid v4 (snappy)"]];
+			const frameworks = origamis;
 
 			// HTML
-			var gridHtml = "<div id=\"grid-displayer\" style=\"display: none;\"><div class=\"gd-container\"><div class=\"gd-row\"></div></div></div>",
-			gridToolsHtml = "<div id=\"grid-displayer-tools\">";
+			const gridHtml = "<div id=\"grid-displayer\" style=\"display: none;\"><div class=\"gd-container\"><div class=\"gd-row\"></div></div></div>";
+			let gridToolsHtml = "<div id=\"grid-displayer-tools\">";
 			gridToolsHtml += "	<div class=\"gdt-field\"><select id=\"gdt-framework\">";
 			gridToolsHtml += "		<option>&darr; Choose your framework</option>";
 			$.each(frameworks, function(index, value) {
 				gridToolsHtml += "<option value=\"" + value[0] + "\"";
-				gridToolsHtml += (value[0] === gdFramework) ? " selected" : "";
+				gridToolsHtml += value[0] === gdFramework ? " selected" : "";
 				gridToolsHtml += ">" + value[1] + "</option>";
 			});
-			gridToolsHtml += "";
+			gridToolsHtml = String(gridToolsHtml);
 			gridToolsHtml += "	</select></div>";
 			gridToolsHtml += "	<div id=\"gdt-options\" class=\"gdt-field\">";
 			gridToolsHtml += "	  <div><label for=\"gdt-color\">Columns colour</label><input type=\"text\" id=\"gdt-color\" value=\"" + gdColor + "\" /></div>";
@@ -165,8 +165,8 @@
 		}
 	};
 
-	var head = document.getElementsByTagName("head")[0];
-	var gridScript = document.createElement("script");
+	const head = document.getElementsByTagName("head")[0];
+	const gridScript = document.createElement("script");
 	gridScript.src = "https://www.ft.com/__origami/service/build/v2/bundles/js?modules=o-grid@^5.0.0";
 	gridScript.onload = init();
 	head.appendChild(gridScript);
@@ -174,8 +174,8 @@
 	// Load jQuery from CDN if needed
 	function init() {
 		if (!window.jQuery) {
-			var jQueryScript = document.createElement("script");
-			jQueryScript.src  = "https://code.jquery.com/jquery-3.3.0.min.js";
+			const jQueryScript = document.createElement("script");
+			jQueryScript.src = "https://code.jquery.com/jquery-3.3.0.min.js";
 			jQueryScript.onload = function() { startBookmarklet(window.jQuery); };
 			head.appendChild(jQueryScript);
 		} else {

fe7533f89dadd596781b2db511bdc6ae67e5aa82
chee <chee@snoot.club>
2021-09-03 15:56:46 +0100

    chore: add more thorough test.mjs script

---
+++ scripts/test.mjs
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+import {$} from "zx"
+import * as workspaces from "./workspaces.mjs"
+import {readPackage} from "read-pkg"
+
+let sleep = async seconds => new Promise(wakeup => setTimeout(wakeup, seconds * 1000))
+
+async function hasScript(workspace, name) {
+	return typeof (await readPackage({cwd: workspace})).scripts?.[name] == "string"
+}
+
+for await (let workspace of workspaces.paths()) {
+	process.stdout.write(`\n\n${workspace}:\n`)
+	
+	if (await hasScript(workspace, "lint")) {
+		await `npm run -w ${workspace} lint`
+	} else {
+		await $`npm exec -w ${workspace} -- origami-build-tools verify` 
+	}
+
+	if (await hasScript(workspace, "test")) {
+		await `npm run -w ${workspace} test`
+	} else {
+		await $`npm exec -w ${workspace} -- origami-build-tools test` 
+	}
+
+	await sleep(10)
+}
+
+export {}

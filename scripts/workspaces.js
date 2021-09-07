3224d91fca7e47621ba329fbff88bfd3527a05c2
chee <chee@snoot.club>
2021-09-07 03:23:24 +0100

    chore: add sort script

--- scripts/workspaces.js
+++ scripts/workspaces.js
@@ -1,6 +1,7 @@
 import {$} from "zx"
 import {globby as glob} from "globby"
 import {readPackage} from "read-pkg"
+import toposort from "toposort"
 
 let pkgJson = await readPackage()
 let workspaces = Array.isArray(pkgJson.workspaces)
@@ -9,10 +10,39 @@ let workspaces = Array.isArray(pkgJson.workspaces)
 
 export const paths = () => glob(workspaces, {onlyDirectories: true})
 
-export const packages = async function (workspaces = paths()) {
-	let packages = []
-	for (let cwd of await workspaces) {
-		packages.push(await readPackage({cwd}))
+/**
+ *
+ * @param {string[]=} workspaces paths of workspaces to sort
+ * @returns {Promise<string[]>} toposorted workspaces
+ */
+export async function sort(workspaces) {
+	let allWorkspaces = await paths()
+	if (!workspaces) workspaces = allWorkspaces
+	let names = new Set()
+	let namePathMap = {}
+	let pathPackageMap = {}
+	let edges = []
+
+	for (let workspace of allWorkspaces) {
+		let pkg = await readPackage({cwd: workspace})
+		if (workspaces.includes(workspace)) {
+			names.add(pkg.name)
+		}
+		namePathMap[pkg.name] = workspace
+		pathPackageMap[workspace] = pkg
 	}
-	return packages
+
+	for (let path of workspaces) {
+		let pkg = pathPackageMap[path]
+
+		let deps = Object.keys(pkg.peerDependencies || {}).filter(dep => {
+			return names.has(dep)
+		})
+
+		edges.push(...deps.map(dep => [dep, pkg.name]))
+	}
+
+	return toposort.array(Array.from(names), edges).map(n => {
+		return namePathMap[n]
+	})
 }
